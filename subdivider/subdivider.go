d5863ec55a93daa5c2f3fd2e329af68070774098
theskibb <kristianskibrek@protonmail.com>
2025-05-29 17:28:31 +0200

    subdivider program to chop big markdown into smaller

--- subdivider/subdivider.go
+++ subdivider/subdivider.go
@@ -27,22 +27,34 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	fileToWriteTo := ""
+	strToWrite := ""
 
 	for scanner.Scan() {
 		line := scanner.Text()
 		if len(line) > 1 && line[:2] == "# " {
+			if strToWrite != "" {
+				writeToFile(fileToWriteTo, strToWrite)
+				strToWrite = ""
+			}
+
 			fileToWriteTo = line[2:]
 			fmt.Println(fileToWriteTo)
 		} else if fileToWriteTo == "" {
 			fmt.Println("skipping line")
 			continue
-		} else {
-			fmt.Println("writing line")
-			lineBytes := []byte(line)
-			err := os.WriteFile(fileToWriteTo, lineBytes, 0644)
-			if err != nil {
-				log.Fatal("cannot write file", err)
-			}
 		}
+		strToWrite += line + "\n"
+	}
+
+	if fileToWriteTo != "" && strToWrite != "" {
+		writeToFile(fileToWriteTo, strToWrite)
+	}
+
+}
+
+func writeToFile(fileToWriteTo, strToWrite string) {
+	err := os.WriteFile(fileToWriteTo, []byte(strToWrite), 0644)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
